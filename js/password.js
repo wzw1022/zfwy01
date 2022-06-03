bbc5ca37c75b139c9311c32f59fd2d3cd04e4973
liqi0101 <105900055+liqi0101@users.noreply.github.com>
2022-06-03 14:28:16 +0800

    Update password.js

--- js/password.js
+++ js/password.js
@@ -1,48 +1,50 @@
 // 1.验证手机号是否合法
 // 1.获取输入框内容
+var phonez_ = /^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$/;
+var mma = /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9~!@&%#_]{8,16}$/;
 var t1 = document.getElementById('t1');
 var t2 = document.getElementById('t2');
 var t3 = document.getElementById('t3');
 var t4 = document.getElementById('t4');
 // var agg = 0;
 
-t1.oninput = function () {
-    var arr_ = /^1[3578]\d{9}$/;
-    // if (!arr_.test(t1.value)) {
-    //     alert('请输入正确的手机号');
-    // } else {
-    //     alert('输入正确')
+// t1.oninput = function () {
+//     var arr_ = /^1[3578]\d{9}$/;
+//     // if (!arr_.test(t1.value)) {
+//     //     alert('请输入正确的手机号');
+//     // } else {
+//     //     alert('输入正确')
 
 
-    // }
-}
+//     // }
+// }
 
-t2.oninput = function () {
-    // if (t2.value == obj) {
-    //     alert('验证码错误')
-    // } else {
-    //     alert('输入正确')
+// t2.oninput = function () {
+//     // if (t2.value == obj) {
+//     //     alert('验证码错误')
+//     // } else {
+//     //     alert('输入正确')
 
-    // }
-}
+//     // }
+// }
 
-t3.oninput = function () {
-    var arr = /^\w{6,10}$/;
-    if (!arr.test(t3.value)) {
+// t3.oninput = function () {
+//     var arr = /^\w{6,10}$/;
+//     if (!arr.test(t3.value)) {
 
-    } else {
+//     } else {
 
-    }
-}
+//     }
+// }
 
-t4.oninput = function () {
-    if (t4.value != t3.value) {
+// t4.oninput = function () {
+//     if (t4.value != t3.value) {
 
-    } else {
+//     } else {
 
-    }
-}
-
+//     }
+// }
+var su = 0;
 var tableText = document.getElementsByClassName('getCode')[0];
 var obj;
 var timer;
@@ -67,12 +69,35 @@ tableText.onclick = function () {
                 obj.push(num)
             }
             obj = obj.join('');
+            su = obj;
+            console.log(su);
             alert('你的验证码为：' + obj);
         }
     }, 1000)
 }
-
+console.log(su);
 var submit = document.getElementById('submit');
-submit.onclick = function () {
-    submit.innerHTML = '注册成功'
-}
+touch.on(submit, 'tap', function () {
+    if (t1.value == '') {
+        alert('手机号不能为空！');
+    } else if (t2.value == '') {
+        alert('验证码不能为空！');
+    } else if (t3.value == '') {
+        alert('密码不能为空！');
+    } else if (t4.value == '') {
+        alert('确认密码不能为空！');
+    } else if (!phonez_.test(t1.value)) {
+        alert('手机号格式错误');
+    } else if (t2.value != su) {
+        alert('验证码错误');
+    } else if (!mma.test(t3.value)) {
+        alert('密码格式错误，请输入8-16位并带有1个大写和小写字母');
+    } else if (t3.value != t4.value) {
+        alert('两次密码不一致！');
+    } else {
+        localStorage.setItem("phone", t1.value);
+        localStorage.setItem("password", t3.value);
+        alert('找回密码成功,即将跳转到登录页面');
+        setTimeout(window.location.href = 'login.html', 2);
+    }
+})
